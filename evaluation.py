25a4ba08a55868dcc1b7b563102eaafa3187e7aa
Michael Talaga <61634676+mtalaga20@users.noreply.github.com>
2023-08-15 19:51:04 -0400

    Delete evaluation.py

--- evaluation.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def cross_entropy(predicted_val:int, actual_val:int) -> int:
-    return -((actual_val * math.log(predicted_val) + (1-actual_val) * math.log(1-predicted_val)))
-
-
-
-#Validation - print(cross_entropy(0.6, 1))
